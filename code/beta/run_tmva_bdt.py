8ec8d0a354b3ad0abb8932527f230f362964e3ae
Jordan Ashley <jashley6@vols.utk.edu>
2026-01-13 12:38:14 -0800

    :sparkles: Variable list to dict, plot labeling, axis limit resolution

--- code/beta/run_tmva_bdt.py
+++ code/beta/run_tmva_bdt.py
@@ -1,6 +1,13 @@
 import ROOT
 from ROOT import TMVA
+from dataclasses import dataclass
 from array import array
+
+# Manual normalization
+def normalize_in_place(h):
+    integ = h.Integral()
+    if integ > 0:
+        h.Scale(1.0 / integ)
 
 # Create output file
 output_file = ROOT.TFile("TMVA_output.root", "RECREATE")
@@ -11,35 +18,44 @@ factory = TMVA.Factory("TMVAClassification", output_file,
         "!V:!Silent:Color:DrawProgressBar:Transformations=I;:AnalysisType=Classification")
 dataloader = TMVA.DataLoader("dataset")
 
-# Define input variables
-variables = [
-    "Cluster_ArrivalTime",
-    "Cluster_EnergyDeposited",
-    "Incident_Angle",
-    "Cluster_Size_x",
-    "Cluster_Size_y",
-    "Cluster_Size_tot",
-    "Cluster_x",
-    "Cluster_y",
-    "Cluster_z",
-    "Cluster_RMS_x",
-    "Cluster_RMS_y",
-    "Cluster_Skew_x",
-    "Cluster_Skew_y",
-    "Cluster_AspectRatio",
-    "Cluster_Eccentricity"
-]
+@dataclass(frozen=True)
+class Variable:
+    label: str
+    ymax: float
+    xmin: float
+    xmax: float
+
+variables = {
+        "Cluster_ArrivalTime": Variable(label="Cluster arrival time [ns]", ymax=0.23, xmin=-0.52, xmax=0.7),
+        "Cluster_EnergyDeposited": Variable(label="Cluster energy deposited [KeV]", ymax=1.1, xmin=0, xmax=0.0007),
+        "Incident_Angle": Variable(label="Incident angle", ymax=0.077, xmin=0, xmax=3),
+        "Cluster_Size_x": Variable(label="Cluster size in x [pixels]", ymax=1.1, xmin=0, xmax=60),
+        "Cluster_Size_y": Variable(label="Cluster size in y [pixels]", ymax=1.1, xmin=0, xmax=400),
+        "Cluster_Size_tot": Variable(label="Total cluster size [pixels]", ymax=1.1, xmin=0, xmax=400),
+        "Cluster_x": Variable(label="Cluster x position [cm]", ymax=0.11, xmin=-35, xmax=35),
+        "Cluster_y": Variable(label="Cluster y position [cm]", ymax=0.11, xmin=-35, xmax=35),
+        "Cluster_z": Variable(label="Cluster z position [cm]", ymax=0.0055, xmin=-80, xmax=80),
+        "Cluster_RMS_x": Variable(label="Cluster RMS in x [cm^2]", ymax=0.22, xmin=0, xmax=90000),
+        "Cluster_RMS_y": Variable(label="Cluster RMS in y [cm^2]", ymax=0.22, xmin=0, xmax=350000),
+        "Cluster_Skew_x": Variable(label="Cluster skew in x", ymax=0.55, xmin=-1.75, xmax=1.75),
+        "Cluster_Skew_y": Variable(label="Cluster skew in y", ymax=0.55, xmin=-1.75, xmax=1.75),
+        "Cluster_AspectRatio": Variable(label="Cluster aspect ratio", ymax=1.1, xmin=0, xmax=25000),
+        "Cluster_Eccentricity": Variable(label="Cluster eccentricity", ymax=1.1, xmin=0.85, xmax=1)
+        }
 
 # Load input variables
-for v in variables:
-    dataloader.AddVariable(v, "F")
+for v_id, _ in variables.items():
+    dataloader.AddVariable(v_id, "F")
 for i in range(9):
     dataloader.AddVariable(f"PixelHits_EnergyDeposited_{i}", "F")
     dataloader.AddVariable(f"PixelHits_ArrivalTime_{i}", "F")
 
 # Load signal and background files
-sig_file = ROOT.TFile("/global/cfs/projectdirs/atlas/jashley/mjolnir/data/beta/MAIA/signal/Hits_TTree_output_digi_light_training.root")
-bkg_file = ROOT.TFile("/global/cfs/projectdirs/atlas/jashley/mjolnir/data/beta/MAIA/bg/Hits_TTree_output_digi_light_training.root")
+#sig_file = ROOT.TFile("/global/cfs/projectdirs/atlas/jashley/mjolnir/data/beta/MAIA/signal/Hits_TTree_output_digi_light_training.root")
+#bkg_file = ROOT.TFile("/global/cfs/projectdirs/atlas/jashley/mjolnir/data/beta/MAIA/bg/Hits_TTree_output_digi_light_training.root")
+sig_file = ROOT.TFile("/global/cfs/projectdirs/atlas/jashley/mjolnir/data/beta/MAIA/Hits_TTree_output_digi_noMS_5kMuons_10_170Theta_0_5TeV_1.root")
+bkg_file = ROOT.TFile("/global/cfs/projectdirs/atlas/jashley/mjolnir/data/beta/MAIA/Hits_TTree_MAIAvxb75_noMS_1_0-0.root")
+
 sig_tree = sig_file.Get("HitTree")
 bkg_tree = bkg_file.Get("HitTree")
 
@@ -59,38 +75,43 @@ factory.TrainAllMethods()
 factory.TestAllMethods()
 factory.EvaluateAllMethods()
 
+# --- INPUT VARIABLE DISTRIBUTIONS --- #
 n_bins = 50
+ROOT.gStyle.SetOptStat(0)
 
-for v in variables:
-    # Determine common range from both trees
-    sig_min = sig_tree.GetMinimum(v)
-    sig_max = sig_tree.GetMaximum(v)
-    bkg_min = bkg_tree.GetMinimum(v)
-    bkg_max = bkg_tree.GetMaximum(v)
+for v_id, v in variables.items():
+    x_min = v.xmin
+    x_max = v.xmax
+    y_max = v.ymax
 
-    x_min = min(sig_min, bkg_min)
-    x_max = max(sig_max, bkg_max)
+    h_sig_name = f"h_sig_{v_id}"
+    h_bkg_name = f"h_bkg_{v_id}"
 
-    h_sig_name = f"h_sig_{v}"
-    h_bkg_name = f"h_bkg_{v}"
-
-    h_sig = ROOT.TH1F(h_sig_name, f"Normalized {v} (signal vs background)", n_bins, x_min, x_max)
-    h_bkg = ROOT.TH1F(h_bkg_name, f"Normalized {v} (signal vs background)", n_bins, x_min, x_max)
+    h_sig = ROOT.TH1F(h_sig_name, f"Normalized {v.label} (signal vs background)", n_bins, x_min, x_max)
+    h_bkg = ROOT.TH1F(h_bkg_name, f"Normalized {v.label} (signal vs background)", n_bins, x_min, x_max)
 
     # Fill hists from original trees
-    sig_tree.Draw(f"{v}>>{h_sig_name}", "", "goff")
-    bkg_tree.Draw(f"{v}>>{h_bkg_name}", "", "goff")
+    sig_tree.Draw(f"{v_id}>>{h_sig_name}", "", "goff")
+    bkg_tree.Draw(f"{v_id}>>{h_bkg_name}", "", "goff")
 
     # Pretty plots
-    ROOT.gStyle.SetOptStat(0)
-    c = ROOT.TCanvas(f"c_{v}", f"{v} signal vs background", 800, 600)
+    c = ROOT.TCanvas(f"c_{v_id}", f"{v_id} signal vs background", 800, 600)
 
     h_sig.SetLineColor(ROOT.kRed)
     h_bkg.SetLineColor(ROOT.kBlue)
     h_sig.SetLineWidth(2)
     h_bkg.SetLineWidth(2)
-    h_sig.DrawNormalized("HIST")
-    h_bkg.DrawNormalized("HIST SAME")
+    
+    # Fix axis issues by normalizing manually
+    normalize_in_place(h_sig)
+    normalize_in_place(h_bkg)
+
+    h_sig.SetMaximum(y_max)
+    h_sig.GetXaxis().SetTitle(f"{v.label}")
+    h_sig.GetYaxis().SetTitle("Normalized number of clusters")
+
+    h_sig.Draw("HIST")
+    h_bkg.Draw("HIST SAME")
 
     leg = ROOT.TLegend(0.7, 0.75, 0.9, 0.9)
     leg.AddEntry(h_sig, "Signal", "l")
